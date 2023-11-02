2f760c381a388897d96780428fd8ecfa59c42602
semyon.zhulikov <semyon.zhulikov@latoken.com>
2023-11-02 22:06:19 +0300

    add logs

--- api/webhook.ts
+++ api/webhook.ts
@@ -1,9 +1,17 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  console.log(req, res);
-  const { name = "World" } = req.query;
-  return res.json({
-    message: `Hello ${name}!`,
-  });
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  console.log(req);
+  if (req.headers["stripe-signature"]) {
+    const event = req.body; // Входящее событие от Stripe
+    const userId = event.data.object.customer; // Идентификатор пользователя из вебхука
+
+    console.log(event, userId);
+
+    // Выполните необходимые манипуляции с пользователем, используя userId
+
+    return res.status(200).json({ received: true });
+  } else {
+    return res.status(400).json({ error: "Invalid request" });
+  }
 }
